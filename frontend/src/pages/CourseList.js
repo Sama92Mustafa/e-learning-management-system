d599caddfac522e6df6d98770a8b4088b47d54af
mk <mohammedkadar30@gmail.com>
2024-11-29 19:52:14 -0500

    Update CourseList.js

--- frontend/src/pages/CourseList.js
+++ frontend/src/pages/CourseList.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getCourses } from '../api/api';
+import { getCourses, updateCourse} from '../api/api';
 
 const CourseList = () => {
   const [courses, setCourses] = useState([]);
+  const [ isEditing, setIsEditing] = useState(false);
+  const [ currentCourse, setCurrentCourse] = useState({ title: '', description: '' });
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -11,13 +13,70 @@ const CourseList = () => {
     };
     fetchCourses();
   }, []);
+  const handleEditClick = (course) => {
+    setCurrentCourse(course);
+    setIsEditing(true);
+  };
+const handleInputChange = (e) => {
+    const { name, value} = e.target;
+    setCurrentCourse({...currentCourse, [name]: value});
+  };
 
+  const handleUpdate = async () => {
+    try {
+      await updateCourse(currentCourse._id, currentCourse);
+      alert('Course has been updated successfully');
+      setIsEditing(false);
+      setCourses((prevCourses) => 
+      prevCourses.map((course) => 
+        course._id === currentCourse._id ? {...currentCourse } : course
+    )
+  );
+  } catch (error) {
+    console.error(error);
+    alert('Failed to update course'); 
+  }
+  };
+  
   return (
+    <div>
     <ul>
       {courses.map((course) => (
-        <li key={course.id}>{course.title}</li>
+        <li key={course._id}>
+          {course.title} - {course.description}
+          <button onClick={() => handleEditClick(course)}>Edit</button>
+          </li>
       ))}
     </ul>
+
+    {isEditing && (
+    <div className="modal"> 
+    <h3>Edit Course</h3>
+    <label> 
+      Title:
+      <input
+      type="text"
+      name="title"
+      value={currentCourse.title}
+      onChange={handleInputChange}
+      />
+    </label>
+
+    <label>
+      Description:
+      <input
+       type="text"
+       name="description"
+       value={currentCourse.description}
+       onChange={handleInputChange}
+       />
+    </label>
+
+    <button onClick={handleUpdate}>Save</button>
+    <button onClick={() => setIsEditing(false)}>Cancel</button>
+    </div>
+   )}
+   </div>
   );
 };
 
