4781ded762de69027cbee80f94547198f60e02b1
dariagrigor <dariagrigoro@gmail.com>
2024-11-29 22:55:15 -0500

    navigation links added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import Navigation from './components/Navigation';
 
 
 
